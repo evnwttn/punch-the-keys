58e69345e9bab26c762035bea3edd39a4b616c28
Jack Arthur Harrhy <me@jackharrhy.com>
2021-09-07 20:24:36 -0230

    sept7th fixes

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 // KEYBOARD
 
 document.body.addEventListener("keydown", (e) => {
-  e.preventDefault();
-  let key = getKey(e);
-  if (!key) {
-    return console.warn("No key for", e.keyCode);
-  }
-  key.setAttribute("data-pressed", "on");
+	e.preventDefault();
+	let key = getKey(e);
+	if (!key) {
+		return console.warn("No key for", e.keyCode);
+	}
+	key.setAttribute("data-pressed", "on");
 });
 
 document.body.addEventListener("keyup", (e) => {
-  e.preventDefault();
-  let key = getKey(e);
-  key && key.removeAttribute("data-pressed");
+	e.preventDefault();
+	let key = getKey(e);
+	key && key.removeAttribute("data-pressed");
 });
 
 function getKey(e) {
-  let location = e.location;
-  let selector;
-  if (location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
-    selector = ['[data-right="true"]', '[data-key="' + e.keyCode + '"]'].join(
-      ""
-    );
-  } else {
-    let code = e.keyCode || e.which;
-    selector = [
-      '[data-key="' + code + '"]',
-      '[data-char*="' + encodeURIComponent(String.fromCharCode(code)) + '"]',
-    ].join(",");
-  }
-  return document.querySelector(selector);
+	let location = e.location;
+	let selector;
+	if (location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
+		selector = ['[data-right="true"]', '[data-key="' + e.keyCode + '"]'].join(
+			""
+		);
+	} else {
+		let code = e.keyCode || e.which;
+		selector = [
+			'[data-key="' + code + '"]',
+			'[data-char*="' + encodeURIComponent(String.fromCharCode(code)) + '"]',
+		].join(",");
+	}
+	return document.querySelector(selector);
 }
 
 function pressKey(char) {
-  let key = document.querySelector('[data-char*="' + char.toUpperCase() + '"]');
-  if (!key) {
-    return console.warn("No key for", char);
-  }
-  key.setAttribute("data-pressed", "on");
-  setTimeout(function () {
-    key.removeAttribute("data-pressed");
-  }, 200);
+	let key = document.querySelector('[data-char*="' + char.toUpperCase() + '"]');
+	if (!key) {
+		return console.warn("No key for", char);
+	}
+	key.setAttribute("data-pressed", "on");
+	setTimeout(function () {
+		key.removeAttribute("data-pressed");
+	}, 200);
 }
 
 // SYNTH
@@ -48,71 +48,75 @@ function pressKey(char) {
 let oscType = ["sawtooth", "triangle", "square", "sine"];
 let oscNum = 0;
 
-let synth = new Tone.PolySynth(Tone.Synth, {
-  oscillator: {
-    type: `${oscType[oscNum]}`,
-  },
-}).toDestination();
+function makeSynth(oscillatorType) {
+	return new Tone.PolySynth(Tone.Synth, {
+		oscillator: {
+			type: oscillatorType,
+		},
+	}).toDestination();
+}
+
+let synth = makeSynth(oscType[oscNum]);
 
 document.body.addEventListener("keydown", (e) => {
-  let getOsc = getKey(e);
-  if (getOsc.hasAttribute("osc-up")) {
-    if (oscNum <= 2) {
-      oscNum++;
-    }
-    console.log("ya");
-  } else if (getOsc.hasAttribute("osc-down")) {
-    if (oscNum > 0) {
-      oscNum--;
-    }
-    console.log("ya");
-  }
+	let getOsc = getKey(e);
+
+	if (getOsc.hasAttribute("osc-up")) {
+		if (oscNum <= oscType.length - 1) {
+			oscNum++;
+			synth = makeSynth(oscType[oscNum]);
+		}
+		console.log("ya");
+	} else if (getOsc.hasAttribute("osc-down")) {
+		if (oscNum > 0) {
+			oscNum--;
+			synth = makeSynth(oscType[oscNum]);
+		}
+		console.log("ya");
+	}
 });
 
 // OCTAVE FUNCTIONS
 
 let octave = 4;
 
+function handleOctaveElement(elm) {
+	if (elm.hasAttribute("octave-up")) {
+		octave++;
+	} else if (elm.hasAttribute("octave-down")) {
+		octave--;
+	} else if (elm.hasAttribute("octave")) {
+		octave = elm.getAttribute("octave");
+	}
+}
+
 document.body.addEventListener("keydown", (e) => {
-  let getOctave = getKey(e);
-  if (getOctave.hasAttribute("octave-up")) {
-    octave++;
-  } else if (getOctave.hasAttribute("octave-down")) {
-    octave--;
-  } else if (getOctave.hasAttribute("octave")) {
-    octave = getOctave.getAttribute("octave");
-  }
+	let getOctave = getKey(e);
+	handleOctaveElement(getOctave);
 });
 
-const clickOctave = document.querySelectorAll(".gdt");
-for (let i = 0; i < clickOctave.length; i++) {
-  clickOctave[i].addEventListener("click", function () {
-    if (clickOctave[i].hasAttribute("octave-up")) {
-      octave++;
-    } else if (clickOctave[i].hasAttribute("octave-down")) {
-      octave--;
-    } else if (clickOctave[i].hasAttribute("octave")) {
-      octave = clickOctave[i].getAttribute("octave");
-    }
-  });
-}
+document.querySelectorAll(".gdt").map((clickOctave) =>
+	clickOctave.addEventListener("click", () => {
+		handleOctaveElement(clickOctave[i]);
+	})
+);
 
 // KEY FUNCTIONS
 
 document.body.addEventListener("keydown", (e) => {
-  let typeKey = getKey(e);
-  if (typeKey.hasAttribute("data-sound")) {
-    let note = typeKey.getAttribute("data-sound");
-    synth.triggerAttackRelease(`${note}${octave}`, "4n");
-  }
+	let typeKey = getKey(e);
+	if (typeKey.hasAttribute("data-sound")) {
+		let note = typeKey.getAttribute("data-sound");
+		synth.triggerAttackRelease(`${note}${octave}`, "4n");
+	}
 });
 
 const clickKey = document.querySelectorAll(".gdt");
 for (let i = 0; i < clickKey.length; i++) {
-  clickKey[i].addEventListener("click", function () {
-    if (clickKey[i].hasAttribute("data-sound")) {
-      let note = clickKey[i].getAttribute("data-sound");
-      synth.triggerAttackRelease(`${note}${octave}`, "4n");
-    }
-  });
+	clickKey[i].addEventListener("click", function () {
+		if (clickKey[i].hasAttribute("data-sound")) {
+			let note = clickKey[i].getAttribute("data-sound");
+			synth.triggerAttackRelease(`${note}${octave}`, "4n");
+		}
+	});
 }
