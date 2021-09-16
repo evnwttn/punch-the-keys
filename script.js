a3bad9aaeca46c328fca1407ebaeb590507cf028
evnwttn <evnwttn@gmail.com>
2021-09-16 19:45:00 -0230

    PUNCH THE KEYS

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// TITLE
+
+anime
+  .timeline({ loop: false })
+  .add({
+    targets: "#ptfk .word",
+    scale: [14, 1],
+    opacity: [0, 1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i,
+  })
+  .add({
+    targets: "#ptfk",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000,
+  });
+
 // KEYBOARD
 
 document.body.addEventListener("keydown", (e) => {
