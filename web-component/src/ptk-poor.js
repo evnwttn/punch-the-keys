9fff0d2ad772d5e26437e1411baff056e3861605
evnwttn <evnwttn@gmail.com>
2021-10-15 11:33:55 -0230

    :octopus:

--- web-component/src/ptk-poor.js
+++ web-component/src/ptk-poor.js
@@ -2,87 +2,87 @@ import { LitElement, html } from "lit";
 import * as Tone from "tone";
 
 class PoorMansPunchTheKeys extends LitElement {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.currentNote = 'none';
-		this.octave = 4;
+    this.currentNote = "none";
+    this.octave = 4;
 
-		this.oscType = ["sawtooth", "triangle", "square", "sine"];
-		this.synth = this.makeSynth(this.oscType[0]);
+    this.oscType = ["sawtooth", "triangle", "square", "sine"];
+    this.synth = this.makeSynth(this.oscType[0]);
 
-		this.rows = [
-			{ sound: "C" },
-			{ sound: "D" },
-			{ sound: "E" },
-			{ sound: "F" },
-			{ sound: "G" },
-			{ sound: "A" },
-			{ sound: "B" },
-		];
-	}
+    this.rows = [
+      { sound: "C" },
+      { sound: "D" },
+      { sound: "E" },
+      { sound: "F" },
+      { sound: "G" },
+      { sound: "A" },
+      { sound: "B" },
+    ];
+  }
 
-	render() {
-		return html`
-			<p>poor mans punch the keys</p>
-			${this.currentNote}
-			<br />
-			${this.oscType.map(
-				(oscType) => html`
-					<button @click="${this.onOscTypeClick}" data-osc-type="${oscType}">
-						${oscType}
-					</button>
-				`
-			)}
-			<br />
-			${this.rows.map(
-				(row) => html`
-					<button @click="${this.onClickButton}" data-sound="${row.sound}">
-						${row.sound}
-					</button>
-				`
-			)}
-		`;
-	}
+  render() {
+    return html`
+      <p>poor mans punch the keys</p>
+      ${this.currentNote}
+      <br />
+      ${this.oscType.map(
+        (oscType) => html`
+          <button @click="${this.onOscTypeClick}" data-osc-type="${oscType}">
+            ${oscType}
+          </button>
+        `
+      )}
+      <br />
+      ${this.rows.map(
+        (row) => html`
+          <button @click="${this.onClickButton}" data-sound="${row.sound}">
+            ${row.sound}
+          </button>
+        `
+      )}
+    `;
+  }
 
-	makeSynth(synthType) {
-		return new Tone.PolySynth(Tone.Synth, {
-			oscillator: {
-				type: synthType,
-			},
-			volume: this.volume,
-		}).toDestination();
-	}
+  makeSynth(synthType) {
+    return new Tone.PolySynth(Tone.Synth, {
+      oscillator: {
+        type: synthType,
+      },
+      volume: this.volume,
+    }).toDestination();
+  }
 
-	onOscTypeClick(event) {
-		const oscType = event.target.dataset.oscType;
+  onOscTypeClick(event) {
+    const oscType = event.target.dataset.oscType;
 
-		if (!oscType) {
-			throw new Error(
-				"button was clicked, but it lacked a data-osc-type attribute"
-			);
-		}
+    if (!oscType) {
+      throw new Error(
+        "button was clicked, but it lacked a data-osc-type attribute"
+      );
+    }
 
-        this.synth = this.makeSynth(oscType);
-	}
+    this.synth = this.makeSynth(oscType);
+  }
 
-	onClickButton(event) {
-		const sound = event.target.dataset.sound;
+  onClickButton(event) {
+    const sound = event.target.dataset.sound;
 
-		if (!sound) {
-			throw new Error(
-				"button was clicked, but it lacked a data-sound attribute"
-			);
-		}
+    if (!sound) {
+      throw new Error(
+        "button was clicked, but it lacked a data-sound attribute"
+      );
+    }
 
-		const note = `${sound}${this.octave}`;
+    const note = `${sound}${this.octave}`;
 
-		this.synth.triggerAttackRelease(note, "8n");
+    this.synth.triggerAttackRelease(note, "8n");
 
-		this.currentNote = note;
-	}
+    this.currentNote = note;
+  }
 }
 PoorMansPunchTheKeys.properties = {
-	currentNote: {},
+  currentNote: {},
 };
 customElements.define("ptk-poor", PoorMansPunchTheKeys);
