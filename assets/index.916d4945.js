7200cbb4fb0923b5f53acb2c47b283ab3d9bcaff
evnwttn <evnwttn@gmail.com>
2021-10-29 13:04:14 +0000

    Deploying to gh-pages from @ evnwttn/punch-the-keys@69bd7967b7d88a9324aeb20b70f63baff65f721e 🚀

--- assets/index.916d4945.js
+++ assets/index.916d4945.js
@@ -500,4 +500,4 @@ PERFORMANCE OF THIS SOFTWARE.
     text-shadow: 1px 1px 8px #0000005e;
     pointer-events: none;
   }
-`;customElements.define("ptk-poor",vi);
+`;customElements.define("ptk",vi);
